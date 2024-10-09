a6ebd804873d93af162426f98984d81c7c3ac4f0
AzarAhmadov <ehmedovh111@gmail.com>
2024-10-09 14:29:24 +0400

    fix: Update AuthService.ts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,19 +1,17 @@
 import axios from "axios";
 import { RegisterSchemaType, LoginSchemaType } from "../schema/zod-schema";
 
-const API_BASE_URL = "https://youngartist.onrender.com/young-artist-app/api/v1";
-
 export const loginUser = async (values: LoginSchemaType) => {
    try {
-      const response = await axios.post(`${API_BASE_URL}/users/login`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/users/login`, {
          ...values,
       });
       if (response.status === 200) {
-         localStorage.setItem("token", response.data.token); 
-       } else {
+         localStorage.setItem("token", response.data.token);
+      } else {
          console.error(response.data.message);
-       }
-       console.log("log data",response )
+      }
+      console.log("log data", response);
       return response;
    } catch (error) {
       console.error("Error during login", error);
@@ -23,7 +21,7 @@ export const loginUser = async (values: LoginSchemaType) => {
 
 export const registerUser = async (values: RegisterSchemaType) => {
    try {
-      const response = await axios.post(`${API_BASE_URL}/users/register`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/users/register`, {
          ...values,
       });
       return response.data;
@@ -36,9 +34,9 @@ export const registerUser = async (values: RegisterSchemaType) => {
 // Logout user
 export const logoutUser = () => {
    localStorage.removeItem("token");
- };
- 
- // Check authentication
- export const checkAuth = () => {
+};
+
+// Check authentication
+export const checkAuth = () => {
    return localStorage.getItem("token") !== null;
- };
+};
